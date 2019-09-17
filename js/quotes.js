9d89aad3f6b2c1762e9552dbcf6003c58d9070d2
Stephanie Adamkiewicz <stephanie.adamkiewicz@gmail.com>
2019-09-17 09:44:56 -0700

    Add starter files

---
+++ js/quotes.js
@@ -0,0 +1,64 @@
+let quotes = [
+  {
+    quote: "Take the first step in faith. You don't have to see the whole staircase, just take the first step.",
+    source: 'Martin Luther King Jr.',
+    citation: ,
+    year:
+  },
+  {
+    quote: "I do not wish you much happiness—it would bore you; I do not wish you trouble either; but, following the people’s philosophy, I will simply repeat: ‘Live more,’ and try somehow not to be too bored; this useless wish I am adding on my own.",
+    source: 'Fyodor Dostoyevsky',
+    citation: 'The Posessed',
+    year: 1871
+  },
+  {
+    quote: "The only difference between a madman and myself is that I am not mad.",
+    source: "Salvidor Dali",
+    citation: ,
+    year: 1942
+  },
+  {
+    quote: 'No good fish goes anywhere without a porpoise.',
+    source: 'Lewis Carroll',
+    citation: "Alice's Adventures in Wonderland",
+    year: 1866
+  },
+  {
+    quote: "The limits of my lanugage mean the limits of my world.",
+    source: 'Ludwig Wittgenstein',
+    citation: "Logico-Tractatus Philosophicus",
+    year: 1921
+  },
+  {
+    quote: "Every man is born as many men and dies as a single one.",
+    source: "Martin Heidegger",
+    citation: ,
+    year:
+  },
+  {
+    quote: "He was going to live forever, or die in the attempt.",
+    source: "Joseph Heller",
+    citation: "Catch-22",
+    year: 1961
+  },
+  {
+    quote: "Who controls the past controls the future. Who controls the present controls the past.",
+    source: "George Orwell",
+    citation: "1984",
+    year: 1949
+  },
+  {
+    quote: "We are what we pretend to be, so we must be careful about what we pretend to be.",
+    source: "Kurt Vonnegut",
+    citation: "Mother Night",
+    year: 1961
+  },
+  {
+    quote: "Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less.",
+    source: "Marie Skłodowska Curie",
+    citation: "Our Precarious Habitat",
+    year: 1973
+  }
+]
+
+console.log(quotes)
---
+++ js/script.js
@@ -0,0 +1,50 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+console.log(quotes)
+
+//returns a random quote object from the quotes array
+function getRandomQuote () {
+  let numOfQuotes = quotes.length
+  let rando = Math.floor(Math.random() * numOfQuotes) + 1
+  return quotes[rando]
+}
+/***
+  Create the `getRandomQuote` function to:
+   - Create a variable to store a random number
+   - Cse the random number to `return` a random quote object from the `quotes` array.
+***/
+
+
+
+
+/***
+  Create the `printQuote` function to:
+   - Call the `getRandomQuote` function and assign it to a variable.
+   - Create a variable for the HTML string and set it equal to an empty string.
+   - Use the HTML template in the instructions or the markup in the index.html file, AND
+     the random quote vairable to build your HTML string.
+   - Add the quote and source section to the HTML string.
+   - Use an if statement to check for the citation property before adding it to the HTML string.
+   - Use an if statement to check for the year property before adding it to the HTML string.
+   - Don't forget to close that final `p` tag.
+   - Set the `innerHTML` of the `quote-box` div to the HTML string.
+***/
+
+
+
+
+/***
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
+  comment.
+***/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
